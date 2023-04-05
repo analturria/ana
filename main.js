07063c72fe23a4b62924b1730f9eb9cc498d4d19
analturria <129889737+analturria@users.noreply.github.com>
2023-04-05 11:56:59 -0300

    teste

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ document.addEventListener("click" , showcolor);
 
 document.getElementById("botao").addEventListener("click" , exibenome)
 
+document.getElementById("btn-somar").addEventListener("click" , somar)
+
 function showcolor()
 {
     document.body.style.backgroundColor = "pink";
@@ -12,4 +14,11 @@ function exibenome()
     let usuario = document.getElementById( "nome" ).value;
     document.getElementById( "mensagem" ).innerHTML = "Meu nome é: " + usuario;
     document.getElementById( "nome" ).value = "";
+}
+
+function somar()
+{
+    let digitoum = document.getElementById("numb1").value;
+    let digitodois = document.getElementById("numb2").value;  
+    document.getElementById("resultado1").innerHTML = document.getElementById("numb1").innerHTML + document.getElementById("numb2").innerHTML
 }

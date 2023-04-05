847ce16851c46b38f5f72f7ce9b8156dda99ab64
analturria <129889737+analturria@users.noreply.github.com>
2023-04-05 11:27:40 -0300

    teste html e js

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 document.addEventListener("click" , showcolor);
 
+document.getElementById("botão").addEventListener("click" , exibenome)
+
 function showcolor()
 {
     document.body.style.backgroundColor = "pink";
+}
+
+function exibenome()
+{
+    let usuario = document.getElementById( "nome" ).value;
+    document.getElementById( "mensagem" ).innerText = "Meu nome é:" + usuario
+    document.getElementById( "nome" ).value = "";
 }

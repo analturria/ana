1589492d5000baa67ed7e221d3bdcfe77cb7d9ac
analturria <129889737+analturria@users.noreply.github.com>
2023-04-05 13:53:34 -0300

    teste

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ function exiberesultado()
 function exiberesultado2()
 {
     let n1 = document.getElementById("n1").value;
-    let n1 = document.getElementById("n2").value;  
+    let n2 = document.getElementById("n2").value;  
     document.getElementById("resultado2").innerHTML = parseFloat( n1 ) - parseFloat( n2 );
 }
 
